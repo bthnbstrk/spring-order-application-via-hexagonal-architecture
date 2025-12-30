6989c4061ddf49f275f04f7cc0171261ba7562d4
Batuhan BAŞTÜRK <37836695+bthnbstrk@users.noreply.github.com>
2025-12-30 17:25:34 +0300

    Implementing order application service
    Implementing order application service dto, implementing order domain service

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
@@ -9,6 +9,11 @@ public class Product extends BaseEntity<ProductId> {
     private Money price;
 
 
+    public void updateWithConfirmedNameAndPrice(String name, Money price){
+        this.name = name;
+        this.price = price;
+    }
+
     public Product(ProductId productId,String name, Money price) {
         super.setId(productId);
         this.name = name;
